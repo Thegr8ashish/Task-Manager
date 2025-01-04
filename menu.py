c5ce6300ad42cf2c0bd17c5a89e85f87a3617bf1
Ashish <123179614+Thegr8ashish@users.noreply.github.com>
2025-01-04 20:36:03 +0530

    Update menu.py
    To Forcefully close CMD

--- menu.py
+++ menu.py
@@ -32,7 +32,7 @@ def menu(tasks):
             elif choice == 6:
                 save_tasks(tasks)  # Save tasks before exiting
                 print("Exiting...")
-                sys.exit(0)
+                os.exit(0)
             else:
                 print("Invalid choice, please try again.")
         except ValueError:
